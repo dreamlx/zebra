227c6ef0ca01c891eed1815e8f212f635bf8e5d4
hielf <hielf@qq.com>
2016-03-07 01:04:13 +0800

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -66,7 +66,16 @@ module ApplicationHelper
 
   def qr_code(serial_id)
     qrcode = RQRCode::QRCode.new("#{serial_url(serial_id)}", :size => 4, :level => :h)
-    png = qrcode.as_png
+    png = qrcode.as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 120,
+          border_modules: 4,
+          module_px_size: 6,
+          file: nil # path to write
+          )
     png.save("public/img/qrcode/#{serial_id}.png")
     # IO.write("public/img/qrcode/#{serial_id}.png", png.to_s)
   end
