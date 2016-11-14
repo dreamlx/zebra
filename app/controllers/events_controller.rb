5a708ced1c742af0df649eef6693685d660feff2
hielf <hielf@qq.com>
2016-11-15 01:17:24 +0800

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
+  # skip_load_and_authorize_resource :only => [:new, :create]
+  # skip_before_action :logged_in_admin, :only => [:new, :create]
 
   def index
+    # current_user = Admin.first
     @events = Event.all
     @events_grid = initialize_grid(Event,
       name: 'events_grid')
